ad9f2c306c5eae6806dd82f7356154a230b1dbfb
GuiNo12 <guilherme.nogueiracoelho@live.com>
2022-05-26 14:50:37 -0300

    Adição de tema escuro

--- src/Pages/PaginaInicial/PaginaInicial.jsx
+++ src/Pages/PaginaInicial/PaginaInicial.jsx
@@ -1,9 +1,10 @@
 import { Post } from "../../components/Post/Post";
+import "./PaginaInicial.css";
 
 export function PaginaInicial() {
     return (
         <section>
-            <h1>Página Inicial</h1>
+            <h1 className="texto-primario">Página Inicial</h1>
             <Post nomeUsuario={"Teste"} porcentagem={"50"} postagem={"Post teste"}></Post>
             <Post nomeUsuario={"USR 2"} porcentagem={"40"} postagem={"Post teste 5"}></Post>
             <Post nomeUsuario={"USR 3"} porcentagem={"100"} postagem={"Post teste 4"}></Post>
