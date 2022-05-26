45b38d67108615d4a1f1c334fbdeaf2034e43cec
GuiNo12 <guilherme.nogueiracoelho@live.com>
2022-05-26 11:24:01 -0300

    Adição de ìcones

--- src/components/MenuUsuario/MenuUsuario.jsx
+++ src/components/MenuUsuario/MenuUsuario.jsx
@@ -1,6 +1,10 @@
 import './MenuUsuario.css';
 import { CardPerfil } from "../CardPerfil/CardPerfil";
 import { BotaoCriarPost } from '../BotaoCriarPost/BotaoCriarPost';
+import bookmark from '../../assets/bookmark.png';
+import perfil from '../../assets/perfil-de-usuario.png';
+import comentario from '../../assets/comentario.png';
+import home from '../../assets/home.png';
 
 export function MenuUsuario() {
     return (
@@ -9,25 +13,25 @@ export function MenuUsuario() {
             <ul>
                 <li>
                     <a href="/meusPosts">
-                        <img className="icone-menu" src="https://cdn-icons.flaticon.com/png/512/3031/premium/3031121.png?token=exp=1652482945~hmac=62626fdb7a037e8777cfa6fa044f54e1" alt="imagem de postagens" />
+                        <img className="icone-menu" src={bookmark} alt="imagem de postagens" />
                         Meus Posts
                     </a>
                 </li>
                 <li>
                     <a href="/editarPerfil">
-                        <img className="icone-menu" src="https://cdn-icons.flaticon.com/png/128/3114/premium/3114957.png?token=exp=1652483021~hmac=df538564997955a4e166170bb323b918" alt="imagem de perfil" />
+                        <img className="icone-menu" src={perfil} alt="imagem de perfil" />
                         Editar Perfil
                     </a>
                 </li>
                 <li>
                     <a href="/meusComentarios">
-                        <img className="icone-menu" src="https://cdn-icons.flaticon.com/png/512/3114/premium/3114810.png?token=exp=1652482257~hmac=d26ecf086209ad94b4265444d9740eb1" alt="imagem de comentarios" />
+                        <img className="icone-menu" src={comentario} alt="imagem de comentarios" />
                         Comentários
                     </a>
                 </li>
                 <li>
                     <a href="/paginaInicial">
-                        <img className="icone-menu" src="https://cdn-icons.flaticon.com/png/512/3114/premium/3114810.png?token=exp=1652482257~hmac=d26ecf086209ad94b4265444d9740eb1" alt="imagem de comentarios" />
+                        <img className="icone-menu" src={home} alt="imagem de comentarios" />
                         Pagina Inicial
                     </a>
                 </li>
