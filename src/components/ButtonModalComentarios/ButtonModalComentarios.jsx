f9a55974fa039b1d28bc6e20e42c1fff0b23b267
pablohsgc <pablohsgc@gmail.com>
2022-05-31 15:32:33 -0300

    Retirada de botoes dentro de comentarios

--- src/components/ButtonModalComentarios/ButtonModalComentarios.jsx
+++ src/components/ButtonModalComentarios/ButtonModalComentarios.jsx
@@ -30,12 +30,6 @@ export function ButtonModalComentarios(props){
                   ))
                 }
               </div>
-
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-              </div>
-
             </div>
           </div>
         </div>
